4b6cb205ce5090931a2ebdc400ce5a7e68ab8c24
Ewa Basara <ebasara@wsiz.edu.pl>
2024-10-17 13:11:00 +0200

    Initial commit

---
+++ Lab1/1510.py
@@ -0,0 +1,28 @@
+# Zadanie 3
+"""
+imie = "Ewa"
+kierunek = "Informatyka"
+uczelnia = "WSIZ"
+#print(*objects, sep=' ', end='\n', file=None, flush=False)
+print("Nazywam się " + imie, end=" ")
+print("studiuje na kierunku " + kierunek + " na " + uczelnia)
+"""
+
+# Zadanie 4
+"""
+# input(prompt)
+name = input("Jak ci na imie?")
+print(f"Cześć {name}, witaj na zajęciach UwU")
+"""
+
+# Zadanie 5
+"""
+a = int(input("Podaj pierwszy bok prostokąta: "))
+b = int(input("Podaj drugi bok prostokąta: "))
+print("Pole twojego prostokąta: "+ str(a*b))
+print("Obwód twojego prostokąta: "+ str(2*a+2*b))
+"""
+
+# Zadanie 6
+
+
---
+++ Lab1/1710.py
@@ -0,0 +1,32 @@
+# Zadanie 3
+# print(*objects, sep=' ', end='\n', file=None, flush=False)
+
+"""imie = "Ewa"
+kierunek = "Informatyka"
+uczelnia = "WSIZ"
+
+print("Nazywam się " + imie, end=' ')
+print("Studiuje na kierunku", kierunek, sep=' ')
+# DODAJ BRAKUJĄCY PRINT
+"""
+
+# Zadanie 4
+#input(prompt)
+"""
+print("Jak się nazywasz?")
+name = input()
+print(f"Cześć {name}, witaj na zajęciach UwU")
+"""
+
+# Zadanie 5
+bok1 = float(input("Podaj długość boku prostokąta: "))
+bok2 = float(input("Podaj długość drugiego boku prostokąta: "))
+
+obw = ((bok1+bok2) * 2)
+# pole = a*b
+
+print(obw)
+# "Obwód prostokąta wynosi X, a pole wynosi Y"
+
+# Obliczyć pole
+# Zwróć odpowiednie komunikaty
