df8a60a3e8b79344d6305924c640b230028d4590
BasaraE <ebasara@wsiz.edu.pl>
2024-10-29 13:23:09 +0100

    Gl01 Lab2

--- Lab1/2910.py
+++ Lab1/2910.py
@@ -69,7 +69,64 @@ elif delta > 0:
     print(f"Równanie ma dwa rozwiązania: {rozw}, {rozw2}")
 else:
     print("Równanie jest sprzeczne")
+"""
+
+
+
+
+
+
+
+
+### Lab 2
+# Zadanie 1
+#a
+#for i in range(1, 101):
+#    print(i)
+#b
+#for i in range(100, -1, -1):
+#    print(i)
+#range(7, 78, 7)
+#range(20, -1, -2)
+
+# Zadanie 2
+
+#ile=int(input("Ile lini gwiazdek wyświetlić? "))
+""" A
+for i in range (ile):
+    for i in range (ile):
+        print("*", end=" ")
+    print("")
+"""
+""" B
+for i in range (ile):
+    for j in range (i+1):
+        print("*", end=" ")
+    print("")
+
+# ile = "*" + " " - zawsze 2 znaki :)
+spc = ile - 1
+
+for i in range (ile):
+    print(" " * spc, end="")
+    spc -= 1
+    for j in range (i+1):
+        print("*", end=" ")
+    print("")
+
+
+
+
+#for znak in "spam_napis":
+#    print(znak, end=' ')
 
 """
-123=121
-
+# Zadanie 5
+tekst = "Rzeszów jest piękny"
+print(tekst)
+#print(tekst[6]) #7 LITERA TO INDEX 6
+#print(tekst[0])
+#print(tekst[-1])
+#print(tekst[0 : : 2])
+#print(tekst[10 : ])
+#print(tekst[-1 : : -1])
