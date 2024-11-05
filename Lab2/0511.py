5f829532eb24d223750b824585737e1c56c53649
BasaraE <ebasara@wsiz.edu.pl>
2024-11-05 11:59:39 +0100

    SL01 Lab2

---
+++ Lab2/0511.py
@@ -0,0 +1,21 @@
+#Zadanie 8
+"""
+x=-4
+while x <= 4:
+    y= 2*x*x - 5*x - 8
+    print(f"Dla x= {x}, wartość funkcji wynosi {y}")
+    x+=0.5
+"""
+#Zadanie 9
+while True:
+    liczba = int(input("Podaj liczbe całkowitą: "))
+    if liczba < 0:
+        print("Wychodzę z pętli")
+        break
+
+
+
+
+
+
+
---
+++ Lab3/0511.py
@@ -0,0 +1,26 @@
+#Zadanie 1
+imiona=["Ola", "Tadek", "Janek", "Katia"]
+
+# A
+imiona=sorted(imiona)
+
+# B
+imiona.append("Władek")
+imiona.append("Magda")
+#ona=imiona[-1]
+ona=imiona.pop()
+#print(ona)
+
+#C
+imiona.insert(2, "Gosia")
+
+#D
+imiona.reverse()
+#imiona=imiona*2
+#print(imiona*2)
+
+#for imie in imiona:
+#    print(imie, end=" ")
+
+#Zadanie 4
+
