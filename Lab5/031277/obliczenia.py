eaf670da9c9bba9490ed4b72b61f27e878fa7ea2
ebasara <ebasara@wsiz.edu.pl>
2024-12-05 12:37:24 +0100

    SG01

---
+++ Lab5/031277/obliczenia.py
@@ -0,0 +1,6 @@
+# Importuje caaaałą zawartość pliku geometria
+import geometria3
+from geometria3 import obwod_kola
+
+print(obwod_kola(16))
+print(geometria3.pole_kola(16))
--- Lab5/Zad7/obliczenia.py
+++
@@ -1,6 +0,0 @@
-# Importuje caaaałą zawartość pliku geometria
-import geometria
-from geometria import obwod_kola
-
-print(obwod_kola(16))
-print(geometria.pole_kola(16))
