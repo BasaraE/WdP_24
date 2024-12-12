6eed3e2de5081df8aad06ad4427ed5a9bfd2249d
ebasara <ebasara@wsiz.edu.pl>
2024-12-12 13:16:01 +0100

    sl02

---
+++ Lab4/1212.py
@@ -0,0 +1,20 @@
+"""
+a=68
+def suma():
+    #a=16
+    b=34
+    print(a*2)
+    print(2 * b)
+
+print(2*a)
+#print(2*b)
+print("wewnątrz funkcji: ")
+suma()
+print(2*a)
+
+"""
+
+
+#Zadanie 1
+
+2*wez(p-1)-1
